db224533e759b52a46425b85303ad1b2dec930b2
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-10-12 08:56:42 -0700

    mobile and desktop help

--- src/components/MobileHelp.js
+++ src/components/MobileHelp.js
@@ -1,9 +1,188 @@
-import React from 'react'
+import styled from 'styled-components';
+import {useState} from 'react';
+import {BsQuestionCircleFill} from 'react-icons/bs';
+import mobileImg from '../images/mobileHelp.jpg'
+import {MdChatBubble} from 'react-icons/md'
+import {BsFillTelephoneFill} from 'react-icons/bs'
+
+
 
 const MobileHelp = () => {
+  const [openHelp, setOpenHelp] = useState(false)
+ 
   return (
-    <div>MobileHelp</div>
+    <Wrapper>
+        <div className='mobile-help-btn'>
+            <button className={`${openHelp ? 'noShow' : 'btn'}`}        
+                    onClick={() => setOpenHelp(true)}>
+                <BsQuestionCircleFill />
+            </button> 
+        </div>
+        {openHelp && 
+          <article className='mobile-help-container'>
+            <div className="close-btn-container">
+              <button className='close-btn' onClick={() => setOpenHelp(false)}>X</button>
+            </div>
+            <div className='mobile-help-header'>
+                <img src={mobileImg} alt="technical assistant" />
+                <div>
+                  <p>Contact Us Today!</p>
+                  <p>We're Here To Help!</p>
+                </div>
+             </div>
+             <div className='mobile-help-content'>
+                <ul>
+                  <li><a href="!#">Track Orders</a> </li>
+                  <li><a href="!#">Returns</a> </li>
+                  <li><a href="!#">Store Locator</a> </li>
+                  <li><a href="!#">11 Music Lessons</a> </li>
+                </ul>
+
+                <button className='help-btn'>
+                  <MdChatBubble /><span> Live Chat</span>
+                </button>
+                
+                <button className='help-btn'>
+                  <BsFillTelephoneFill /><span> 888-214-4811</span>
+                </button>
+
+                <button className='help-btn'>
+                  Request a callback
+                </button>
+
+                <div className='help-hours'>
+                  <p>MON - FRI 5 AM - 9 PM PT</p>
+                  <p>SAT - SUN 6 AM - 7 PM PT</p>
+                </div>
+              </div>
+              
+
+            </article>
+        
+          
+        }
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+
+    /*  start of question button  */
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        z-index: 500;
+
+      .btn {
+        font-size: 3.5rem;
+        border: none;
+        border-radius: 50%;
+        height: 1em;
+        background: #fff;
+        margin: 0 .25em .5em 0;
+        cursor: pointer ;
+      }
+      .noShow {
+        display: none;
+      }
+    /*  end of question button  */
+
+
+    /*  start of help popup  */
+   .mobile-help-container {
+    position: fixed;
+    bottom: 0;
+    right: 10px;
+    background: white ;
+    width: 300px;
+    height: 600px;
+    margin-bottom: 10px;
+    box-shadow: 0px 0px 4px rgba(0,0,0,0.2);
+   }
+
+   .close-btn-container {
+      height: 50px;
+      background: var(--clr-grey-800);
+    }
+    .close-btn {
+      width: 100%;
+      color: white;
+      background: transparent;
+      border: none;
+      font-size: 1.5rem;
+      cursor: pointer;
+      display: grid;
+      justify-content: end;
+      padding: 8px 20px 0 0;
+    }
+
+    .mobile-help-header img{
+      position: relative;
+      height: 90px;
+      width: 100%;
+      object-fit: cover;
+    }
+    .mobile-help-header div {
+      position: absolute;
+      top: 75px;
+      left: 140px;
+      text-align: center;
+    }
+    .mobile-help-header div p {
+      margin-bottom: 0;
+    }
+    .mobile-help-content {
+      padding: 20px;
+    }
+    ul {
+      padding-bottom: 1.5em;
+    }
+    ul li {
+      font-size: 1rem;
+      padding-bottom: .25em;
+      color: red;
+    }
+    ul li:hover {
+      text-decoration: underline;
+    }
+    a {
+      color: red;
+    }
+
+    .help-btn{
+      width: 100%;
+      background: red ;
+      color: white;
+      border: none;
+      font-size: 1.3rem;
+      padding: .5em;
+      border-radius: 4px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      margin-bottom: 1em;
+    }
+    .help-btn span {
+      padding-left: .5em;
+      font-size: 1rem;
+    }
+    .help-hours {
+      font-size: .875rem;
+      text-align: center;
+    }
+    .help-hours p {
+       margin-bottom: .25em;
+    }
+
+
+    @media (min-width: 960px){
+        .mobile-help-btn {
+            display: none;
+        }
+    }
+
+`
+
 
 export default MobileHelp
--- src/components/ModalHelpDesktop.js
+++ src/components/ModalHelpDesktop.js
@@ -1,9 +1,135 @@
-import React from 'react'
+import styled from 'styled-components'
+import {BsFillTelephoneFill} from 'react-icons/bs'
+import {MdChatBubble} from 'react-icons/md'
 
-const ModalHelpDesktop = () => {
+const Modal = ({closeModal}) => {
   return (
-    <div>ModalHelpDesktop</div>
+ <Wrapper>
+    <div className="modal-background">
+      <div className="modal-container">
+          <div className='modal-close-btn-container'>
+            <button className='close-btn' onClick={() => closeModal(false)}>X</button>
+          </div>
+          <div className="modal-content">
+            <div className='modal-content-intro'>
+              <p>Hey there.</p>
+              <p>We want to help you out.</p>
+              <p>Hit us up with your gear questions</p>
+            </div>
+            <div className="modal-content-contact">
+              <p>Give us a call</p>
+              <button className='btn'>
+                <BsFillTelephoneFill /><span> 888-214-4811</span>
+              </button>
+              <button className='btn'>
+                  Request a callback
+              </button>
+              <p className='modal-spanish'>Espa&#241;ol <span>877-784-5544</span></p>
+            </div>
+            <div className='modal-chat'>
+                <p>Live Chat</p>
+                <button className='btn'>
+                  <MdChatBubble /><span> Live Chat</span>
+              </button>
+              <p>Hours:</p>
+              <p>MON - FRI 5 AM - 9 PM PT</p>
+              <p>SAT - SUN 6 AM - 7 PM PT</p>
+
+            </div>
+          </div>
+      </div>
+    </div>
+  </Wrapper> 
   )
 }
 
-export default ModalHelpDesktop
+const Wrapper = styled.main`
+
+.modal-background {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(31, 41, 55, 0.5);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 900 !important;
+}
+.modal-container {
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 420px;
+  height: 100vh;
+  
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.35);
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-close-btn-container {
+  height: 50px;
+  background: var(--clr-grey-800);
+}
+.close-btn {
+  width: 100%;
+  color: white;
+  background: transparent;
+  border: none;
+  font-size: 1.5rem;
+  cursor: pointer;
+  display: grid;
+  justify-content: end;
+  padding: 8px 20px 0 0;
+}
+
+.modal-content {
+  margin: 0 auto;
+  max-width: 80%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.modal-content-intro {
+  padding-top: 2em;
+}
+
+.modal-content-intro p{
+  text-align: center;
+  margin-bottom: 0;
+  color: var(--clr-grey-600);
+}
+
+.modal-content-contact, .modal-chat {
+  text-align: center;
+  padding-top: 3em;
+  width: 100%;
+}
+
+.btn{
+  width: 100%;
+  background: red ;
+  color: white;
+  border: none;
+  font-size: 1.3rem;
+  padding: .5em;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin-bottom: 1.5em;
+}
+.btn span {
+  padding-left: .5em;
+  font-size: 1rem;
+}
+.modal-spanish {
+  color: red;
+}
+
+`
+
+export default Modal
