06de34adab1c87f7e9bc42894afaaee722fcde19
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-10-12 08:34:49 -0700

    utils and actions

---
+++ src/utils/misc.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+      id: 1,
+      url: "/guitars",
+      text: "guitars"
+    },
+    {
+      id: 2,
+      url: "/amps",
+      text: "amps"
+    },
+    {
+      id: 3,
+      url: "/pedals",
+      text: "pedals"
+    }
+  ];
+  
