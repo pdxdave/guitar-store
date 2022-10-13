48bb434ca0b917f203c13e63dfac1570e048cefd
David Martinson <davidmartinson@Davids-Mac-mini.local>
2022-10-12 23:04:33 -0700

    lessons md and lrg

--- src/components/Lessons.js
+++ src/components/Lessons.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
-import lessons from '../images/lessons.png'
+import lessons_lrg from '../images/lessons_lrg.png'
+import lessons_md from '../images/lessons_md.png'
 
 const Lessons = () => {
   return (
     <Wrapper className='page-setting'>
-        <img src={lessons} alt="lessons" />
+        <img className="lessons_lrg" src={lessons_lrg} alt="lessons" />
+        <img className="lessons_md" src={lessons_md} alt="lessons" />
     </Wrapper>
   )
 }
@@ -16,5 +18,22 @@ padding-top: 0px;
         width: 100%;
         height: auto;
     }
+    .lessons_lrg {
+        display: none;
+    }
+
+    @media (min-width: 901px){
+        .lessons_lrg {
+            display: grid;
+        }
+        .lessons_md {
+            display: none;
+        }
+        
+    }
+    
+    
+
+
 `
 export default Lessons
